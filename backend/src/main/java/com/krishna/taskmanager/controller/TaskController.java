cf2078d16d0a0687d59f0fbee7c981a8d99958c5
Rajput <Krishna.Rajput@motherson.com>
2026-09-09 11:06:12 +0530

    Allow production frontend CORS

--- backend/src/main/java/com/krishna/taskmanager/controller/TaskController.java
+++ backend/src/main/java/com/krishna/taskmanager/controller/TaskController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ghosttask-frontend.onrender.com")
 public class TaskController {
 
   private final TaskService taskService;
